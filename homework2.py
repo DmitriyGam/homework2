620bf2f2d0f1bb7257f0e0fb08036ecd42a3626e
DmitriyGam <gama4ka@bk.ru>
2024-07-27 22:46:46 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,5 @@
+number_of_completes_HA = '12'
+number_of_hours_spent = "1.5"
+nameCourse = 'Python'
+time_for_one_task = (1.5 / 12)
+print('Курс: ', nameCourse, ", всего задач:", number_of_completes_HA,", затрачено часов:", number_of_hours_spent, ', среднее время выполнение:', time_for_one_task, 'часов')
